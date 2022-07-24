8f9b775af3bc7ad8508fad24fb2c273c9ca3ed00
Liasu Hassan <htope68@gmail.com>
2022-07-24 18:21:18 +0100

    toggle reminder

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -4,6 +4,12 @@ import {Task} from '../Task';
 import { Observable, of } from 'rxjs';
 import{HttpClient, HttpHeaders} from '@angular/common/http'
 
+const httpOptions = {
+   headers : new HttpHeaders({
+     'Content-Type' : 'application/json'
+   })
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -23,5 +29,12 @@ export class TaskService {
     const url = `${this.apiUrl}/${task.id}`;
 
     return this.http.delete<Task>(url)
+  }
+
+  updateTaskReminder(task : Task) : Observable<Task>{
+
+    const url = `${this.apiUrl}/${task.id}`;
+
+    return this.http.put<Task>(url, task, httpOptions);
   }
 }
